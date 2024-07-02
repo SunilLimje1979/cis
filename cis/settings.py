be8b328f68ae4bc5ad122364c76c5c219a12a637
Pawarshekhar709 <pawarshekhar708@gmail.com>
2024-07-02 11:12:26 +0530

    pdf demo

--- cis/settings.py
+++ cis/settings.py
@@ -85,8 +85,8 @@ CORS_ORIGIN_WHITELIST = [
 
 STATIC_URL = "/cis/static/"
 STATICFILES_DIRS = [
-   BASE_DIR / "cis/static"
-    #  BASE_DIR / "static"
+    #BASE_DIR / "cis/static"
+    BASE_DIR / "static"
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
